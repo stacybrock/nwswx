8cd6510afd91a6e805f92d2b9fc7b9f3894d5c8a
Stacy Brock <brock@oregonstate.edu>
2023-01-11 14:08:59 -0800

    Update Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ import nwswx
 # -- Project information -----------------------------------------------------
 
 project = nwswx.__title__
-copyright = '2019, ' + nwswx.__author__
+copyright = '2019-2023, ' + nwswx.__author__
 author = nwswx.__author__
 
 # The short X.Y version
@@ -63,7 +63,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
